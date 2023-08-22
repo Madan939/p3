29822d46fb70ec603c2f0f1905cda37763658aa3
Madan939 <madantamang939@gmail.com>
2023-08-22 13:24:01 +0545

    update

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,29 +1,48 @@
-import React, {  useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { add } from '../store/listSlice'
+import { add, update } from '../store/listSlice'
 import Show from './Show'
 const Add = () => {
     const products = useSelector(state => state.cardState)
+    const product = useSelector(state => state.cardState.Task)
+    //console.log(product)
     const list = products.cartlist
-    const [data, setdata] = useState("")
+    const [data, setdata] = useState()
     const dispatch = useDispatch()
+    useEffect(() => {
+        if (product && product.task !== undefined) {
+            setdata(product.task);
+        } else {
+            setdata("");
+        }
+    }, [product]);
     function sub(e) {
         e.preventDefault()
-    
+        if (product.id) {
+            //console.log(product.id)
+            dispatch(update({ ...product, task: data }))
+            setdata("")
+        }
+        else {
+            if (data.trim() === '') return
             const newtask = {
                 id: Date.now(),
                 task: data
             }
             dispatch(add(newtask))
             setdata("")
+        }
+
 
     }
     return (
         <>
             <form className='mt-5' onSubmit={sub}>
                 <div className='d-flex justify-content-center mb-4 w-50 m-auto'>
-                    <input type='text' className='form-control' onChange={(e) => setdata(e.target.value)} value={data} />
-                    <button type='submit' className='btn btn-primary m-1'>Add</button>
+                    <input type='text' className='form-control' onChange={(e) => setdata(e.target.value)} value={data || ""} />
+                    {product.task ?
+                        (<button type='submit' className='btn btn-success m-1'>update</button>) :
+                        (<button type='submit' className='btn btn-primary m-1'>Add</button>)}
                 </div>
             </form>
             <p className='h4 text-center text-primary'>Total added task={list.length}</p>
--- src/store/listSlice.js
+++ src/store/listSlice.js
@@ -1,28 +1,39 @@
-const {createSlice}=require("@reduxjs/toolkit")
+const { createSlice } = require("@reduxjs/toolkit")
 
-const cardlist=createSlice({
-    name:"card",
-    initialState:{
-        cartlist:[],
-        Task:{}
+const cardlist = createSlice({
+    name: "card",
+    initialState: {
+        cartlist: [],
+        Task: {}
     },
-    reducers:{
-        add(state,action){
-           // console.log(action.payload)
-            const updatedlist=state.cartlist.concat(action.payload)
-            return {...state,cartlist:updatedlist}
+    reducers: {
+        add(state, action) {
+            // console.log(action.payload)
+            const updatedlist = state.cartlist.concat(action.payload)
+            return { ...state, cartlist: updatedlist }
 
         },
-        remove(state,action){
-           // console.log(action.payload)
-            const updatelist=state.cartlist.filter(list=>list.id!==action.payload.id)
-           return {...state,cartlist:updatelist}
+        remove(state, action) {
+            // console.log(action.payload)
+            const updatelist = state.cartlist.filter(list => list.id !== action.payload.id)
+            return { ...state, cartlist: updatelist }
         },
-        edit(state,action){
-          console.log(state.cartlist.find(item=>item.id===action.payload.id))
-            
+        edit(state, action) {
+            const tastFilter = state.cartlist.find(item => item.id === action.payload.id)
+            //console.log(tastFilter)
+            return { ...state, Task: tastFilter }
+
         },
-    },
+        update(state, action) {
+            //console.log(action.payload)
+            const updatedlist = state.cartlist.map((item) =>
+                item.id === action.payload.id ? { id: action.payload.id, task: action.payload.task } : item
+            );
+            // console.log(updatedlist)
+            return { ...state, cartlist: updatedlist, Task: {} };
+        }
+
+    }
 })
-export const {add,remove,edit,update}=cardlist.actions
-export const cardReducer=cardlist.reducer
+export const { add, remove, edit, update } = cardlist.actions
+export const cardReducer = cardlist.reducer
